12b612167b87cdaf2236423402b0ec118018d5fc
Madhu <madhumitharajinikanthan@gmail.com>
2019-01-23 14:21:09 +0530

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      options: {
+        spawn: false
+      },
+      sass: {
+        files: "scss/*.scss",
+        tasks: ["sass", "postcss", "bsReload:css"]
+      },
+      html: {
+        files: "*.html",
+        tasks: ["bsReload:all"]
+      }
+    },
+
+    sass: {
+      options: {
+        precision: 6,
+        sourceComments: false
+      },
+      dist: {
+        files: {
+          "css/madhu.css": "scss/madhu.scss"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({
+            browsers: ["last 2 versions", "ie 8", "ie 9"]
+          })
+        ]
+      },
+      dist: {
+        files: {
+          "css/madhu.css": "css/madhu.css"
+        }
+      }
+    },
+
+    browserSync: {
+      dev: {
+        options: {
+          server: "./",
+          background: true
+        }
+      }
+    },
+
+    bsReload: {
+      css: {
+        reload: "madhu.css"
+      },
+      all: {
+        reload: true
+      }
+    }
+  });
+
+  // Load dependencies
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-sass");
+
+  // Generate and format the CSS
+  grunt.registerTask("default", ["browserSync", "watch"]);
+};
